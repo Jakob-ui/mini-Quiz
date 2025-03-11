89ed21948754d6aa88752895eb9f70771b3d086e
Jakob Laschober <jakob.laschober@gmail.com>
2025-03-11 11:31:17 +0100

    Setup new Project

---
+++ .angular/cache/19.2.1/app/vite/deps/ios.transition-NNY2E7Y3.js
@@ -0,0 +1,15 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-M2JABVQW.js";
+import "./chunk-3ZYCRE32.js";
+import "./chunk-JHUVP4MF.js";
+import "./chunk-XN5KYYNJ.js";
+import "./chunk-ACUVEYEP.js";
+import "./chunk-VEV5BJ2D.js";
+import "./chunk-OKPK4C3D.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
